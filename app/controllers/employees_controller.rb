13ea0a8edf28e0c9d405fe6867f2022f76e4bf02
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-22 12:49:07 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,58 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, except: [:index, :new, :create]
+
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def edit
+        @employee
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            redirect_to new_employee_path(@employee), form_error: @employee.errors.full_messages
+        end
+    end
+
+    def update
+
+        if @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else
+            redirect_to edit_employee_path(@employee), form_error: @employee.errors.full_messages
+        end
+    end
+
+    # button not implemented in view page; not required as per deliverables 
+    def destroy
+        @employee.destroy
+
+        redirect_to employees_path
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog, :img_url)
+    end
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
+    
 end
